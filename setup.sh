44502b8c23c21d150fa406dfa2a64f841f329a37
Your Name <you@example.com>
2021-03-19 15:21:59 +0300

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+GAZEBO_BIN_PATH=`which gazebo`
+GAZEBO_PREFIX=$(dirname $(dirname $GAZEBO_BIN_PATH))
+
+if [ -f ${GAZEBO_PREFIX}/share/gazebo/setup.sh ]; then
+  . ${GAZEBO_PREFIX}/share/gazebo/setup.sh
+elif [ -f /usr/share/gazebo/setup.sh ]; then
+  . /usr/share/gazebo/setup.sh
+else
+  echo "Warning: failed to find Gazebo's setup.sh.  You will need to source it manually."
+fi
+
+export GAZEBO_RESOURCE_PATH=$DIR:${GAZEBO_RESOURCE_PATH}
+
+export GAZEBO_MODEL_PATH=$DIR/models:${GAZEBO_MODEL_PATH}
+
+export GAZEBO_PLUGIN_PATH=$DIR/plugins:${GAZEBO_PLUGIN_PATH}
+export LD_LIBRARY_PATH=$DIR/msgs:${LD_LIBRARY_PATH}
+
+#gazebo --verbose $DIR/worlds/maze.world
